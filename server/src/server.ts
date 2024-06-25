37e73ecc886ee9ded52f6a5d58d9966b9b3052f6
AndreySotnik <dante-sin@mail.ru>
2024-06-25 20:35:53 +0300

    Убрал диагностику из клиентского расширения

--- server/src/server.ts
+++ server/src/server.ts
@@ -96,37 +96,37 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 
 	let problems = 0;
 	let diagnostics: Diagnostic[] = [];
-	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-		problems++;
-		let diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: `${m[0]} is all uppercase.`,
-			source: 'ex'
-		};
-		// if (hasDiagnosticRelatedInformationCapability) {
-		// 	diagnostic.relatedInformation = [
-		// 		{
-		// 			location: {
-		// 				uri: textDocument.uri,
-		// 				range: Object.assign({}, diagnostic.range)
-		// 			},
-		// 			message: 'Spelling matters'
-		// 		},
-		// 		{
-		// 			location: {
-		// 				uri: textDocument.uri,
-		// 				range: Object.assign({}, diagnostic.range)
-		// 			},
-		// 			message: 'Particularly for names'
-		// 		}
-		// 	];
-		// }
-		diagnostics.push(diagnostic);
-	}
+	// while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
+	// 	problems++;
+	// 	let diagnostic: Diagnostic = {
+	// 		severity: DiagnosticSeverity.Warning,
+	// 		range: {
+	// 			start: textDocument.positionAt(m.index),
+	// 			end: textDocument.positionAt(m.index + m[0].length)
+	// 		},
+	// 		message: `${m[0]} is all uppercase.`,
+	// 		source: 'ex'
+	// 	};
+	// 	// if (hasDiagnosticRelatedInformationCapability) {
+	// 	// 	diagnostic.relatedInformation = [
+	// 	// 		{
+	// 	// 			location: {
+	// 	// 				uri: textDocument.uri,
+	// 	// 				range: Object.assign({}, diagnostic.range)
+	// 	// 			},
+	// 	// 			message: 'Spelling matters'
+	// 	// 		},
+	// 	// 		{
+	// 	// 			location: {
+	// 	// 				uri: textDocument.uri,
+	// 	// 				range: Object.assign({}, diagnostic.range)
+	// 	// 			},
+	// 	// 			message: 'Particularly for names'
+	// 	// 		}
+	// 	// 	];
+	// 	// }
+	// 	diagnostics.push(diagnostic);
+	// }
 
 	// Send the computed diagnostics to VSCode.
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
